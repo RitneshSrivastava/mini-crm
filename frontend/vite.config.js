f798c5d9f1cc09ae22f4b402419da86de0580381
Ritnesh Srivastava <122106494+RitneshSrivastava@users.noreply.github.com>
2025-06-03 20:13:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import netlify from 'vite-plugin-netlify';
 
-export default {
-  plugins: [netlify()],
-};
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    netlify()
+  ],
+});
